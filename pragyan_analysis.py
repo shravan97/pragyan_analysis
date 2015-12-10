207079545b5b72993bfdbb62188ca22974f6319b
shravan97 <shravanmurali@gmail.com>
2015-12-10 17:21:11 +0530

    made a slow slide show kinda stuff with 10sec delay

--- pragyan_analysis.py
+++ pragyan_analysis.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker , scoped_session
+import time , os
 
 engine = create_engine('mysql://root:Lordsvn_97@localhost/dummy')
 Session = scoped_session(sessionmaker(bind=engine))
@@ -13,8 +14,19 @@ result_form = my_session.execute("SELECT `form_elementid` , `form_elementname` F
 
 result_form_data = my_session.execute("SELECT `form_elementdata` FROM `form_elementdata` WHERE `page_modulecomponentid`=0 AND `form_elementid`=9;").fetchall()
 #The above query gets the year of student . This has to be done for all form attributes
-
-print result_form
+#0,1,4,6,9,10,13,
+flag=0
+i=0
+arr=[0,1,4,6,9,10]
+print "\t \t *****PARTICIPANTS ANALYSIS*****"
+for i in arr :
+	result_form_data = my_session.execute("SELECT count(form_elementdata) , form_elementdata FROM `form_elementdata` WHERE `page_modulecomponentid`=0 AND `form_elementid`="+str(i)+" GROUP BY form_elementdata;").fetchall()
+	os.system('clear')
+	for j in result_form_data :
+		print j[1] , " : " , j[0]
+	time.sleep(10) 
+	
+#print result_form
 
 """ ToDo : 
 
