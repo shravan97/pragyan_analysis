1db8252f1ecdeeae1383ae1fb744571ffcef02a9
shravan97 <shravanmurali@gmail.com>
2015-12-10 23:06:39 +0530

    added algorithm as comments

--- pragyan_analysis.py
+++ pragyan_analysis.py
@@ -6,27 +6,40 @@ engine = create_engine('mysql://root:Lordsvn_97@localhost/dummy')
 Session = scoped_session(sessionmaker(bind=engine))
 my_session = Session()
 
-result = my_session.execute("SELECT `page_modulecomponentid` FROM `pragyanV3_pages` WHERE `page_name` LIKE 'registration' AND  `page_module` LIKE 'form';").fetchall()
-
-result_form = my_session.execute("SELECT `form_elementid` , `form_elementname` FROM `form_elementdesc` WHERE `page_modulecomponentid`=0;").fetchall()
-#The above query is for registration page at pragyan site
+result = my_session.execute("SELECT `page_modulecomponentid` , `page_parentid` FROM `pragyanV3_pages` WHERE `page_name` LIKE 'registration' AND  `page_module` LIKE 'form';").fetchall()
 
 
-result_form_data = my_session.execute("SELECT `form_elementdata` FROM `form_elementdata` WHERE `page_modulecomponentid`=0 AND `form_elementid`=9;").fetchall()
-#The above query gets the year of student . This has to be done for all form attributes
-#0,1,4,6,9,10,13,
-flag=0
-i=0
-arr=[0,1,4,6,9,10]
-print "\t \t *****PARTICIPANTS ANALYSIS*****"
-for i in arr :
-	result_form_data = my_session.execute("SELECT count(form_elementdata) , form_elementdata FROM `form_elementdata` WHERE `page_modulecomponentid`=0 AND `form_elementid`="+str(i)+" GROUP BY form_elementdata;").fetchall()
-	os.system('clear')
-	for j in result_form_data :
-		print j[1] , " : " , j[0]
-	time.sleep(10) 
+for k in range(len(result)):
+	
+	result_form = my_session.execute("SELECT `form_elementid` , `form_elementname` FROM `form_elementdesc` WHERE `page_modulecomponentid`="+ str(result[k][1]) +";").fetchall()
+	#The above query is for registration page at pragyan site
+	
+	print len(result_form)
+	time.sleep(5)
+	result_form_data = my_session.execute("SELECT `form_elementdata` FROM `form_elementdata` WHERE `page_modulecomponentid`=0 AND `form_elementid`=9;").fetchall()
+	#The above query gets the year of student . This has to be done for all form attributes
+	#0,1,4,6,9,10,13,
+	flag=0
+	i=0
+	arr=[0,1,4,6,8,9,10]
+	
+	for i in arr :
+		result_form_data = my_session.execute("SELECT count(form_elementdata) , `form_elementdata` FROM `form_elementdata` WHERE `page_modulecomponentid`=0 AND `form_elementid`="+str(i)+" GROUP BY form_elementdata;").fetchall()
+		os.system('clear')
+		for j in result_form_data :
+			print j[1] , " : " , j[0]
+		time.sleep(5) 
 	
 #print result_form
+
+#query from line 14 , hence get page_parentid
+#consider page_modulecomponentid =1
+#select page_name from pragyanV3_pages where page_id=<the page_parentid from query on line 14>;
+#select form_elementname  , form_elementid  from form_elementdesc where page_modulecomponetid=1;
+#select count(user_id) , user_id from form_elementdata where page_modulecomponentid =1 ;<then get the array of user_id s>
+# select count(form_elementdata), form_elementdata  from form_elementdata where page_modulecomponentid =1 and form_elementdata REGEXP '[0-9]+' group by form_elementdata;
+#from the above query get the array of form_elementdata .
+#merge the arrays and then execute arr = list(set(arr))
 
 """ ToDo : 
 
